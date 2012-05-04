c889b231fa7e0b8c91917ab6be6c7e63c14942fd
William Crawford <wccrawford@gmail.com>
2012-05-04 18:57:36 -0400

    Added more pieces of the evaluator, and tests.

--- scheem/evaluator_test.js
+++ scheem/evaluator_test.js
@@ -3,10 +3,33 @@ var assert = require('assert');
 var evalScheem = require('./scheem_evaluator.js').evalScheem;
 
 describe('Interpreter', function() {
+	describe('begin', function() {
+		it('should return only value from a set', function() {
+			assert.deepEqual(
+				evalScheem(['begin', 1], {}),
+				1
+			);
+		});
+
+		it('should return the last value from a set', function() {
+			assert.deepEqual(
+				evalScheem(['begin', 1, 2, 3, 4], {}),
+				4
+			);
+		});
+	});
+	
 	describe('values', function() {
 		it('should return numbers', function() {
 			assert.deepEqual(
 				evalScheem(2, {}),
+				2
+			);
+		});
+
+		it('should return numbers if they\'re the only item in a list', function() {
+			assert.deepEqual(
+				evalScheem([2], {}),
 				2
 			);
 		});
@@ -16,6 +39,33 @@ describe('Interpreter', function() {
 				evalScheem('variable', {variable:'test'}),
 				'test'
 			);
+		});
+
+		it('should define values for new variable names', function() {
+			var env = {};
+			assert.deepEqual(
+				evalScheem(['define', 'variable', 5], env),
+				0
+			);
+			assert.deepEqual(env['variable'], 5);
+		});
+
+		it('should define values for new variable names and return it from begin', function() {
+			var env = {};
+			assert.deepEqual(
+				evalScheem(['begin', ['define', 'variable', 5], 'variable'], env),
+				5
+			);
+			assert.deepEqual(env['variable'], 5);
+		});
+		
+		it('should set! values for new variable names', function() {
+			var env = {variable:6};
+			assert.deepEqual(
+				evalScheem(['set!', 'variable', 5], env),
+				0
+			);
+			assert.deepEqual(env['variable'], 5);
 		});
 	});
 
@@ -45,6 +95,94 @@ describe('Interpreter', function() {
 			assert.deepEqual(
 				evalScheem(['/', 10, 5], {}),
 				2
+			);
+		});
+	});
+
+	describe('conditionals', function() {
+		it('should evaluate < as true when left is <', function() {
+			assert.deepEqual(
+				evalScheem(['<', 1, 3], {}),
+				'#t'
+			);
+		});
+
+		it('should evaluate < as false when left is =', function() {
+			assert.deepEqual(
+				evalScheem(['<', 1, 1], {}),
+				'#f'
+			);
+		});
+
+		it('should evaluate < as false when left is >', function() {
+			assert.deepEqual(
+				evalScheem(['<', 3, 1], {}),
+				'#f'
+			);
+		});
+
+		it('should evaluate = as false when left is <', function() {
+			assert.deepEqual(
+				evalScheem(['=', 1, 3], {}),
+				'#f'
+			);
+		});
+
+		it('should evaluate = as true when left is =', function() {
+			assert.deepEqual(
+				evalScheem(['=', 1, 1], {}),
+				'#t'
+			);
+		});
+
+		it('should evaluate = as false when left is >', function() {
+			assert.deepEqual(
+				evalScheem(['=', 3, 1], {}),
+				'#f'
+			);
+		});
+
+		it('should return first value if conditional is true', function() {
+			assert.deepEqual(
+				evalScheem(['if', ['=', 1, 1], 2, 3], {}),
+				2
+			);
+		});
+		
+		it('should return second value if conditional is false', function() {
+			assert.deepEqual(
+				evalScheem(['if', ['=', 1, 0], 2, 3], {}),
+				3
+			);
+		});
+	});
+
+	describe('arrays', function() {
+		it('should insert a number into an array with cons', function() {
+			assert.deepEqual(
+				evalScheem(['cons', 1, ['quote', [2, 3]]], {}),
+				[1, 2, 3]
+			);
+		});
+
+		it('should insert an array into an array with cons', function() {
+			assert.deepEqual(
+				evalScheem(['cons', ['quote', [1, 2]], ['quote', [3, 4]]], {}),
+				[[1, 2], 3, 4]
+			);
+		});
+
+		it('should return the first item in array with car', function() {
+			assert.deepEqual(
+				evalScheem(['car', ['quote', [[1, 2], 3, 4]]], {}),
+				[1, 2]
+			);
+		});
+
+		it('should return all but the first item in array with cdr', function() {
+			assert.deepEqual(
+				evalScheem(['cdr', ['quote', [[1, 2], 3, 4]]], {}),
+				[3, 4]
 			);
 		});
 	});
--- scheem/scheem_evaluator.js
+++ scheem/scheem_evaluator.js
@@ -18,8 +18,47 @@ var evalScheem = function (expr, env) {
         case '/':
             return evalScheem(expr[1], env) / evalScheem(expr[2], env);
 
+		case 'set!':
+        case 'define':
+            env[expr[1]] = evalScheem(expr[2], env);
+            return 0;
+		
+		case 'begin':
+            var val;
+            for(var i=1; i<expr.length; i++) {
+                val = evalScheem(expr[i], env);
+            }
+            return val;
+		
+		case 'cons':
+            var arr = evalScheem(expr[2]);
+            arr.unshift(evalScheem(expr[1]));
+            return arr;
+        case 'car':
+            var arr = evalScheem(expr[1]);
+            return arr.shift();
+        case 'cdr':
+            var arr = evalScheem(expr[1]);
+            arr.shift();
+            return arr;
+
+		case '<':
+			return (expr[1] < expr[2]) ? '#t' : '#f';
+		case '=':
+			return (expr[1] == expr[2]) ? '#t' : '#f';
+
+		case 'if':
+			if(evalScheem(expr[1]) == '#t') {
+                return evalScheem(expr[2]);
+            } else {
+                return evalScheem(expr[3]);
+            }
+
         case 'quote':
             return expr[1];
+
+		default:
+			return evalScheem(expr[0]);
     }
 };
 
