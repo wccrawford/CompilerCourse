d71c328d500459e946044541690720b862d4ebcb
William Crawford <wccrawford@gmail.com>
2012-05-03 20:52:14 -0400

    Added env.

--- scheem/evaluator_test.js
+++ scheem/evaluator_test.js
@@ -3,6 +3,52 @@ var assert = require('assert');
 var evalScheem = require('./scheem_evaluator.js').evalScheem;
 
 describe('Interpreter', function() {
+	describe('values', function() {
+		it('should return numbers', function() {
+			assert.deepEqual(
+				evalScheem(2, {}),
+				2
+			);
+		});
+
+		it('should return values from variable names', function() {
+			assert.deepEqual(
+				evalScheem('variable', {variable:'test'}),
+				'test'
+			);
+		});
+	});
+
+	describe('math', function() {
+		it('should add 2 numbers', function() {
+			assert.deepEqual(
+				evalScheem(['+', 2, 3], {}),
+				5
+			);
+		});
+
+		it('should subtract 2 numbers', function() {
+			assert.deepEqual(
+				evalScheem(['-', 5, 3], {}),
+				2
+			);
+		});
+
+		it('should multiply 2 numbers', function() {
+			assert.deepEqual(
+				evalScheem(['*', 5, 3], {}),
+				15
+			);
+		});
+
+		it('should divide 2 numbers', function() {
+			assert.deepEqual(
+				evalScheem(['/', 10, 5], {}),
+				2
+			);
+		});
+	});
+
 	describe('quote', function() {
 		it('should parse a quoted number', function() {
 			assert.deepEqual(
--- scheem/scheem_evaluator.js
+++ scheem/scheem_evaluator.js
@@ -3,11 +3,21 @@ var evalScheem = function (expr, env) {
     if (typeof expr === 'number') {
         return expr;
     }
+	if (typeof expr === 'string') {
+		return env[expr];
+	}
+
     // Look at head of list for operation
     switch (expr[0]) {
         case '+':
-            return evalScheem(expr[1], env) +
-                   evalScheem(expr[2], env);
+            return evalScheem(expr[1], env) + evalScheem(expr[2], env);
+        case '-':
+            return evalScheem(expr[1], env) - evalScheem(expr[2], env);
+        case '*':
+            return evalScheem(expr[1], env) * evalScheem(expr[2], env);
+        case '/':
+            return evalScheem(expr[1], env) / evalScheem(expr[2], env);
+
         case 'quote':
             return expr[1];
     }
